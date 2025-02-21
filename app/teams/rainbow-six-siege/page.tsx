87c1f608709f7fde9d6c815a6db0b0b4dbd9fa6d
Sandrooreo <alexiorio123@gmail.com>
2025-02-21 18:34:54 -0500

    R6 team updates

--- app/teams/rainbow-six-siege/page.tsx
+++ app/teams/rainbow-six-siege/page.tsx
@@ -23,16 +23,12 @@ export default function RainbowSixSiegePage() {
           <h3 className="font-semibold mb-2">Competition Schedule</h3>
           <ul className="list-disc pl-5">
             <li>
-              <strong>CR6 (Sept 21 - Early December):</strong> A long-standing
-              collegiate league in NA. One match per week, Default time Fridays 8PM EST.
+              <strong>CR6 (January 27th - April):</strong> A long-standing
+              collegiate league in NA. One match per week, Default time Fridays 8:30PM EST.
             </li>
             <li>
-              <strong>CCL (Sept 16th - Nov 25th):</strong> A new collegiate
+              <strong>CCL (January 27th - April):</strong> A new collegiate
               Canadian league. One match per week, Default time Mondays 8PM EST.
-            </li>
-            <li>
-              <strong>Rally Cry (Sept 28th - Nov 10th):</strong> A Ubisoft sponsored
-              collegiate league. One match per week, Default time Sundays 3PM EST.
             </li>
             <li>
               <strong>Open tournaments:</strong> Participation depends on team
@@ -71,12 +67,6 @@ export default function RainbowSixSiegePage() {
       <h2 className="text-2xl font-semibold mb-4">Results</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 mb-8">
         <ResultCard
-          tournamentName="CR6 Spring 2024"
-          logo="/rainbow-six-siege/cr6.webp"
-          result="Top 64 Playoffs"
-          link="https://www.faceit.com/en/championship/b1e622f3-0662-41e5-b1f9-3ed453c47099/CollegiateR6%20Season%209%20Stage%202%20Playoffs/standings/column"
-        />
-        <ResultCard
           tournamentName="Vespa X CR6 Invitational 2024"
           logo="/rainbow-six-siege/cr6.webp"
           result="7th Overall"
@@ -88,76 +78,119 @@ export default function RainbowSixSiegePage() {
           result="Top 64 Playoffs"
           link="https://www.faceit.com/en/championship/e407c138-319d-4aea-b520-965940cf839d/CollegiateR6%20S9%20Stage%201%20Playoffs/standings/column"
         />
+        <ResultCard
+          tournamentName="CR6 Spring 2023"
+          logo="/rainbow-six-siege/cr6.webp"
+          result="Top 64 Playoffs"
+          link="https://www.faceit.com/en/championship/b1e622f3-0662-41e5-b1f9-3ed453c47099/CollegiateR6%20Season%209%20Stage%202%20Playoffs/standings/column"
+        />
+        <ResultCard
+          tournamentName="CR6 Fall 2024"
+          logo="/rainbow-six-siege/cr6.webp"
+          result="20th in Groups"
+          link="https://www.faceit.com/en/championship/c8dd5e47-6417-462b-8af4-0d14bc69a337/CollegiateR6%20-%20Season%2010%20-%20Stage%201%20-%20East/standings/leaderboard"
+        />
+        <ResultCard
+          tournamentName="CCLR6 Season 1 Fall 2024"
+          logo="/ccl.webp"
+          result="6th in Group B"
+          link="https://www.opleague.pro/event/4397-CCLR6Season1-groupstage/standing/overview"
+        />
         {/* Add more ResultCards as needed */}
       </div>
 
-      <h2 className="text-2xl font-semibold mb-4">Concordia Burgundy</h2>
+      <h2 className="text-2xl font-semibold mb-4 pt-4 flex items-center">
+        <div className="bg-black rounded-xl p-2">
+          <img src="/concordia.png" alt="Concordia Maroon Logo" className="w-8 h-8" />
+        </div>
+        <span className="ml-2">Concordia Maroon</span>
+      </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
         <TeamMemberCard
           name="Oreo"
-          role="Team Captain"
+          role="Manager"
           icon="/rainbow-six-siege/oreo.png"
         />
         <TeamMemberCard
+          name="Globby"
+          role="Coach"
+          icon="/rainbow-six-siege/globby.png"
+        />
+        <TeamMemberCard
           name="Cey"
-          role="Flex-Entry"
+          role="Captain"
           icon="/rainbow-six-siege/ceybae.png"
         />
         <TeamMemberCard
-          name="DaRango"
-          role="Support"
-          icon="/rainbow-six-siege/rango.png"
-        />
-        <TeamMemberCard
-          name="Batdashflap"
-          role="Support"
-          icon="/rainbow-six-siege/bat.png"
-        />
-        <TeamMemberCard
           name="Yoyo"
-          role="Flex-Support"
+          role="Flex"
           icon="/rainbow-six-siege/yoyo.png"
         />
         <TeamMemberCard
           name="Job"
-          role="Entry-Flex"
+          role="Entry"
           icon="/rainbow-six-siege/job.png"
+        />
+        <TeamMemberCard
+          name="Owzee"
+          role="Support"
+          icon="/rainbow-six-siege/oz.png"
+        />
+        <TeamMemberCard
+          name="Black"
+          role="Support"
+          icon="/rainbow-six-siege/black.png"
+        />
+        <TeamMemberCard
+          name="BallsUnlisted"
+          role="Support"
+          icon="/rainbow-six-siege/ballsunlisted.png"
         />
         {/* Add more TeamMemberCards as needed */}
       </div>
 
 
-      <h2 className="text-2xl font-semibold mb-4 pt-4">Concordia Gold</h2>
+      <h2 className="text-2xl font-semibold mb-4 pt-4 flex items-center">
+        <div className="bg-black rounded-xl p-2">
+          <img src="/concordia-gold.png" alt="Concordia Gold Logo" className="w-8 h-8" />
+        </div>
+        <span className="ml-2">Concordia Gold</span>
+      </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
       <TeamMemberCard
-          name="Tranmaxpower"
-          role="Entry Fragger"
-          icon="/rainbow-six-siege/tran.png"
-        />
-        <TeamMemberCard
-          name="OMIOZZ"
-          role="Support"
-          icon="/rainbow-six-siege/oz.png"
-        />
-        <TeamMemberCard
-          name="Bandojay"
-          role="Flex-Support"
-          icon="/rainbow-six-siege/bandojay.png"
-        />
-        <TeamMemberCard
           name="Kyoshi"
-          role="Support"
+          role="Captain"
           icon="/rainbow-six-siege/kyoshi.png"
         />
         <TeamMemberCard
-          name="RYMFK"
+          name="Shin"
+          role="Coach"
+          icon="/rainbow-six-siege/shin.png"
+        />
+      <TeamMemberCard
+          name="Rango"
+          role="IGL"
+          icon="/rainbow-six-siege/Rango.png"
+        />
+        <TeamMemberCard
+          name="BatDashFlap"
           role="Support"
-          icon="/rainbow-six-siege/rymfk.png"
+          icon="/rainbow-six-siege/bat.png"
         />
         <TeamMemberCard
           name="Seephoun"
-          role="Entry Fragger"
+          role="Entry"
           icon="/rainbow-six-siege/seephoun.png"
+        />
+        <TeamMemberCard
+          name="NickNack"
+          role="Support"
+          icon="/rainbow-six-siege/nicknack.png"
+        />
+        <TeamMemberCard
+          name="Bondurs"
+          role="Flex"
+          icon="/rainbow-six-siege/bondurs.png"
         />
       </div>
     </div>
