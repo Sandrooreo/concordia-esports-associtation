e4c161b3b76b3c865f46885f78de2fcc216a37ff
Sandrooreo <alexiorio123@gmail.com>
2025-02-22 18:03:50 -0500

    val team

--- app/teams/valorant/page.tsx
+++ app/teams/valorant/page.tsx
@@ -7,7 +7,10 @@ import TeamMemberCard from "@/components/Cards/TeamMemberCard";
 export default function ValorantPage() {
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold mb-8">Valorant Team</h1>
+      <h1 className="text-4xl font-bold mb-8 flex items-center">
+        <img src="/games/valorant.png" alt="Valorant Logo" className="w-14 h-14 mr-3" />
+        Valorant Team
+      </h1>
 
       <Card className="mb-8">
         <CardHeader>
@@ -15,9 +18,24 @@ export default function ValorantPage() {
         </CardHeader>
         <CardContent>
           <p className="mb-4">
-          The Concordia Specter Divide team plays in some TBD leagues and
-          tournaments. Will be based on players availabilities and interests.
+          The Concordia Valorant team is a competitive valorant squad
+            representing Concordia University. We have a strong track record in
+            collegiate leagues and are constantly looking to improve and expand
+            our roster with new faces and friends.
           </p>
+          <h3 className="font-semibold mb-2">Competition Schedule</h3>
+          <ul className="list-disc pl-5">
+            <li>
+              <strong>CVAL East(January 25th - Febuary 22nd)</strong>
+            </li>
+            <li>
+              <strong>CCL (January 27th - April)</strong>
+            </li>
+            <li>
+              <strong>Open tournaments:</strong> Participation depends on team
+              availability.
+            </li>
+          </ul>
         </CardContent>
       </Card>
 
@@ -42,6 +60,66 @@ export default function ValorantPage() {
           </Link>
         </CardContent>
       </Card>
+
+      <h2 className="text-2xl font-semibold mb-4 pt-4 flex items-center">
+              <div className="bg-black rounded-xl p-2">
+                <img src="/concordia.png" alt="Concordia Crimson Logo" className="w-8 h-8" />
+              </div>
+              <span className="ml-2">Concordia Crimson</span>
+            </h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
+              <TeamMemberCard
+                name="Malderdoge"
+                role="Player"
+                icon="/valorant/malderdoge.png"
+              />
+              <TeamMemberCard
+                name="Alchemyy"
+                role="Player"
+                icon="/valorant/alchemyy.png"
+              />
+              <TeamMemberCard
+                name="izakazuya"
+                role="Player"
+                icon="/valorant/kazuya.png"
+              />
+              <TeamMemberCard
+                name="Notetr"
+                role="Player"
+                icon="/valorant/notetr.png"
+              />
+              <TeamMemberCard
+                name="Owzee"
+                role="Player"
+                icon="/valorant/oz.png"
+              />
+              <TeamMemberCard
+                name="Huntxr_"
+                role="Player"
+                icon="/valorant/huntxr.png"
+              />
+              <TeamMemberCard
+                name="Mel_001"
+                role="Sub"
+                icon="/valorant/mel.png"
+              />
+              <TeamMemberCard
+                name="Chrislablitz"
+                role="Analyst and Sub"
+                icon="/valorant/chris.png"
+              />
+              <TeamMemberCard
+                name="Ronnyrdy"
+                role="Coach"
+                icon="/valorant/ronnyrdy.png"
+              />
+              <TeamMemberCard
+                name="Thrillaval"
+                role="Coach"
+                icon="/valorant/thrillaval.png"
+              />
+              {/* Add more TeamMemberCards as needed */}
+            </div>
     </div>
   );
 }
