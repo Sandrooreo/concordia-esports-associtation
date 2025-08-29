1536aafb6b538065a7bf10761d0cb857fd779d34
Sandrooreo <alexiorio123@gmail.com>
2025-08-29 16:19:00 -0400

    kyoshi update

--- app/teams/rainbow-six-siege/page.tsx
+++ app/teams/rainbow-six-siege/page.tsx
@@ -154,7 +154,7 @@ export default function RainbowSixSiegePage() {
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
       <TeamMemberCard
-          name="Kyoshi"
+          name="Kyo"
           role="Captain"
           icon="/rainbow-six-siege/kyoshi.png"
         />
