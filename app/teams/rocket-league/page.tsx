c75d7c6a3de889a42b9a0428551268d82268c26f
Sandrooreo <alexiorio123@gmail.com>
2025-09-29 12:10:28 -0400

    RL fix

--- app/teams/rocket-league/page.tsx
+++ app/teams/rocket-league/page.tsx
@@ -83,7 +83,7 @@ export default function RocketLeaguePage() {
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3">
         <TeamMemberCard
-          name="Naqu"
+          name="Nauq"
           role="Player"
           icon="/rainbow-six-siege/nauq.png"
         />
